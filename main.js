e425c4b24be8cf60f17525e61e0e48b705497f0f
Edwin Kwan <edwinkwan@ML-EKWAN-OSX-SF.local>
2016-10-12 14:22:31 -0700

    First commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var https = require('https');
-var querystring = require('querystring');
+var request = require('request-promise');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
@@ -21,16 +21,42 @@ app.get('/', function(request, response) {
   response.render('index');
 });
 
-app.post('/process_get', urlencodedParser, function (req, res) {
+app.post('/create_user', urlencodedParser, function (req, res) {
+
+  var options = {
+      host: 'https://edwindemo.oktapreview.com',
+      port: 443,
+      path: '/api/v1?activate=false',
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'SSWS 00seiYOZWjGKD0vTgY4a5l0ZGNPaEeA2Sg8WSrUXYk'
+      },
+      body: JSON.stringify({
+        profile: {
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          email: req.body.email,
+          login: req.body.email
+        }
+      })
+  };
+
    // Prepare output in JSON format
-   response = {
+   /*response = {
       firstName:req.body.firstName,
       lastName:req.body.lastName
    };
    console.log(response);
    res.end(JSON.stringify(response));
-
+*/
+request(options).then(function (response){
+    res.status(200).json(response);
 })
+.catch(function (err) {
+    console.log(err);
+})
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
