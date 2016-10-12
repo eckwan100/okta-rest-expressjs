48f1e0f3eba76a86d9d2bebac1cf8718b00f552b
Edwin Kwan <edwinkwan@ML-EKWAN-OSX-SF.local>
2016-10-12 14:53:33 -0700

    First commit

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ app.get('/', function(request, response) {
 app.post('/create_user', urlencodedParser, function (req, res) {
 
   var options = {
-      uri: 'https://edwindemo.oktapreview.com/api/v1?activate=false',
+      uri: 'https://edwindemo.oktapreview.com/api/v1/users?activate=false',
       method: 'POST',
       json: true,
       headers: {
