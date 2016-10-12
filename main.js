397a0e5b2b3fd5e75948fdbee6c7a3832eefb353
Edwin Kwan <edwinkwan@ML-EKWAN-OSX-SF.local>
2016-10-12 11:20:07 -0700

    First commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var express = require('express');
-//var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 var app = express();
 // create application/json parser 
 //var jsonParser = bodyParser.json()
